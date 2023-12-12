9c9e24a6d64cb6dded8d1e8f2b95ed867f28eacb
CodingManoj <CodingManoj@gmail.com>
2023-12-11 21:05:17 -0500

    Jenkins-Shared-Library Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,6 +1,27 @@
-def lintChecks() {
-    sh "echo ***** Starting Style Checks *****"
+def lintChecks('component') {
+    sh "echo ***** Starting Style Checks for ${component} *****"
     sh "npm install jslint"
     sh "node_modules/jslint/bin/jslint.js server.js || true"
     sh "echo ***** Style Checks Are Completed *****"
 }
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Lint Checks'){
+                steps {
+                    script {
+                        nodejs.lintChecks("component")
+                    }
+                }
+            }
+            stage('Static Code Analysis') {
+                steps {
+                    sh "echo Starting Static Code Analysis"
+                }
+            }
+        }
+    }
+}
+
