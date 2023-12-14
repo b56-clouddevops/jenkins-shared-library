590f2d28eb9f165b8840deb139c8e672a0ef7f99
CodingManoj <CodingManoj@gmail.com>
2023-12-13 19:52:49 -0500

    Jenkins-Shared-Library Auto Push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -6,7 +6,7 @@ def lintChecks() {
 
 def sonarChecks() {
      sh '''
-     sonar-scanner -Dsonar.host.url=http://172.31.45.101:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
+     sonar-scanner -Dsonar.host.url=http://172.31.45.101:9000 -Dsonar.java.binaries=./target/ -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
 
      '''
 }
