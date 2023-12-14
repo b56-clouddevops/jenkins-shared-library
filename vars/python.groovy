efa7e8c10608370c98d9963cf50521e21d19341e
CodingManoj <CodingManoj@gmail.com>
2023-12-13 20:27:14 -0500

    Jenkins-Shared-Library Auto Push

--- vars/python.groovy
+++ vars/python.groovy
@@ -5,16 +5,12 @@ def lintChecks() {
     sh "echo ***** Style Checks Are Completed for ${COMPONENT} *****"
 }
 
-def sonarChecks() {
-     sh '''
-     sonar-scanner -Dsonar.host.url=http://172.31.45.101:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
-
-     '''
-}
-
 def call() {
     pipeline {
         agent any
+        environment {                      
+            SONAR_CRED = credentials('SONAR_CRED')
+        }
         stages {
             stage('Lint Checks'){
                 steps {
@@ -27,7 +23,7 @@ def call() {
                 steps {
                     script {
                         env.ARGS="-Dsonar.sources=."
-                        sonarChecks()
+                        common.sonarChecks()
                     }
                 }
             }
