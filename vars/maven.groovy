1045f28dc971fca4187a6c2604be5aa026a32187
CodingManoj <CodingManoj@gmail.com>
2023-12-13 19:39:03 -0500

    Jenkins-Shared-Library Auto Push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,6 +4,13 @@ def lintChecks() {
     sh "echo ***** Style Checks Are Completed for ${COMPONENT} *****"
 }
 
+def sonarChecks() {
+     sh '''
+     sonar-scanner -Dsonar.host.url=http://172.31.45.101:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
+
+     '''
+}
+
 def call() {
     pipeline {
         agent any
@@ -17,7 +24,15 @@ def call() {
             }
             stage('Static Code Analysis') {
                 steps {
-                    sh "echo Starting Static Code Analysis"
+                    script {
+                        sonarChecks()
+                    }
+                }
+            }
+            stage('Get the Sonar Result') {
+                steps {
+                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > gates.sh"
+                    sh "./gates.sh admin password ${SONAR_URL} ${COMPONENT}"
                 }
             }
         }
