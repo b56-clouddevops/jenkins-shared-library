0565bcfe0dc9f6b75b4e6080ebaf4a15e37d5c29
CodingManoj <CodingManoj@gmail.com>
2023-12-18 20:25:09 -0500

    Optimizing Jenkins Shared Library Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -66,7 +66,7 @@ def testCases() {
 
 def artifacts() {
      stage('Checking Artifact Release On Nexys') {
-          env.UPLOAD_STATUS = sh(returnStdout: true, script: "curl http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")              print UPLOAD_STATUS         
+          env.UPLOAD_STATUS = sh(returnStdout: true, script: "curl http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")       
           print UPLOAD_STATUS
      }
 
