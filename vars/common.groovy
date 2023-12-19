d1149b6bcbee47b96ab34164d6416253d44a3d98
CodingManoj <CodingManoj@gmail.com>
2023-12-18 20:30:27 -0500

    Optimizing Jenkins Shared Library Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -73,6 +73,7 @@ def artifacts() {
      if(env.UPLOAD_STATUS == "") {
           stage('Generating The Artifacts') {
                if(env.APP_TYPE == "nodejs") {
+                    sh "pwd"
                     sh "ls -ltr"
                     sh "npm install"
                     sh "zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js"
