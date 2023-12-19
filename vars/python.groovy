5c5e0b4b13c919f0e604ac46729a8f4979e15e3a
CodingManoj <CodingManoj@gmail.com>
2023-12-18 19:43:01 -0500

    Optimizing Jenkins Shared Library Auto Push

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,10 +1,3 @@
-def lintChecks() {
-    sh "echo ***** Starting Style Checks for ${COMPONENT} *****"
-    //sh "pip3 install pylint"
-    //sh "pytlint *.py || true"
-    sh "echo ***** Style Checks Are Completed for ${COMPONENT} *****"
-}
-
 def call() {
     pipeline {
         agent any
