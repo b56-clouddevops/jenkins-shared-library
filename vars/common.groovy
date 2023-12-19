2b391f3c84ec008eaf6c838b35ff356fe8ca70a6
CodingManoj <CodingManoj@gmail.com>
2023-12-18 20:50:44 -0500

    Optimizing Jenkins Shared Library Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -88,6 +88,7 @@ def artifacts() {
                else if(env.APP_TYPE == "angularjs") {
                     sh "cd static/"
                     sh "zip -r ../${COMPONENT}-${TAG_NAME}.zip *"
+                    sh "cd ../"
                }
                else {
                     sh "echo Selected Component Type Does Not Exist"
